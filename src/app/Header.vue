a3007c50d14baeda2cec60d44d8e36b8135fba35
zlv-thisF <alivelz@hotmail.com>
2017-01-02 11:35:56 +0800

    del ZIP

--- src/app/Header.vue
+++ src/app/Header.vue
@@ -2,11 +2,12 @@
   <header class="header">
     <p class="header-title">
       <a href="https://github.com/FountainJS/generator-fountain-webapp" target="_blank">
-        Fountain Generator
+        火星时代答题系统Beta1.1
       </a>
     </p>
     <p class="header-date">
-      Generated with FountainJS v0.6.0 on Wed Aug 10 2016 10:13:26 GMT+0200 (CEST)
+      <button v-on:click="tryy">阿拉拉拉拉</button>
+      <!-- Generated with FountainJS v0.6.0 on Wed Aug 10 2016 10:13:26 GMT+0200 (CEST) -->
     </p>
   </header>
 </template>
